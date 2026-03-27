49e7831e642ea09ad136817bd3052f3365fa1d04
alfarruq <mengboyevfarrux01@gmail.com>
2026-03-27 15:30:07 +0500

    rasim bilan text qo'shildi

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ function App() {
 
   return (
     <div className='wrapper' >
-        <h1>Assalamu alaykum</h1>
+      <h1>Assalamu alaykum</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqJ7XBb8aJ9SuwBkSdpnJKjvWIHbr4RNGaww&s" alt="" />
+      <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur nostrum ipsam totam, saepe vero odit repellendus dolorum quos consequatur ducimus tenetur modi doloribus, culpa quia ipsa similique aliquam illo fugit minus, voluptatibus aut! Quisquam omnis, corporis eius maiores alias hic mollitia sequi exercitationem similique excepturi facere ducimus harum illum, laborum consectetur praesentium nesciunt error iusto culpa inventore non veniam quae! Quia magnam aliquam tenetur corrupti quidem id recusandae officiis illum, qui asperiores facere cum, assumenda esse repellat ipsam delectus nam odit accusamus vero repudiandae neque, ullam quos explicabo non. Impedit blanditiis nemo porro deleniti enim distinctio, quia itaque placeat a.</p>
     </div>
   )
 }
